3c790d72a231285bb552049156df665340dfe903
tanvirmdahmed <developertanvirahmed09@gmail.com>
2023-06-12 20:43:32 +0600

    payment history done

--- src/Pages/Dashboard/MyEnrolledClasses/MyEnrolledClasses.jsx
+++ src/Pages/Dashboard/MyEnrolledClasses/MyEnrolledClasses.jsx
@@ -3,6 +3,7 @@ import useTitle from '../../../Hooks/useTitle';
 import useAxiosSecure from '../../../Hooks/useAxiosSecure';
 import { useQuery } from '@tanstack/react-query';
 import useAuth from '../../../Hooks/useAuth';
+import Title from '../../../components/Title/Title';
 
 const MyEnrolledClasses = () => {
     useTitle('Dashboard | My Enrolled Classes')
@@ -18,7 +19,45 @@ const MyEnrolledClasses = () => {
 
     return (
         <div>
-            My Enrolled Classes
+            <Title title='My Enrolled Classes'></Title>
+            <div className="overflow-x-auto">
+                <table className="table">
+                    {/* head */}
+                    <thead>
+                        <tr className='text-lg bg-amber-50'>
+                            <th>SL No.</th>
+                            <th>Details</th>
+                            <th>Instructor Name</th>
+                            <th>Price</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            myEnrolledClasses.map((myEnrolledClass, i) => <tr key={myEnrolledClass._id} className="hover">
+                                <th>{i + 1}</th>
+                                <td>
+                                    <div className="flex items-center space-x-3">
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle w-12 h-12">
+                                                <img src={myEnrolledClass.classImage} alt="Avatar Tailwind CSS Component" />
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <div className="font-bold">{myEnrolledClass.className}</div>
+                                        </div>
+                                    </div>
+                                </td>
+                                <td className="font-bold">{myEnrolledClass.instructorName}</td>
+                                <td>${myEnrolledClass.price}</td>
+
+                            </tr>)
+                        }
+
+                    </tbody>
+                </table>
+            </div>
+
+
         </div>
     );
 };
--- src/Pages/Dashboard/PaymentHistory/PaymentHistory.jsx
+++ src/Pages/Dashboard/PaymentHistory/PaymentHistory.jsx
@@ -3,21 +3,22 @@ import useTitle from '../../../Hooks/useTitle';
 import useAxiosSecure from '../../../Hooks/useAxiosSecure';
 import { useQuery } from '@tanstack/react-query';
 import useAuth from '../../../Hooks/useAuth';
+import Title from '../../../components/Title/Title';
 
 const PaymentHistory = () => {
     useTitle('Dashboard | Payment History')
 
     const { user } = useAuth();
     const [axiosSecure] = useAxiosSecure();
-    const { data: myPaymentHistory = [], refetch } = useQuery(['myPaymentHistory'], async () => {
-        const res = await axiosSecure.get(`/myPaymentHistory?email=${user?.email}`)
+    const { data: myPaymentHistories = [], refetch } = useQuery(['myPaymentHistories'], async () => {
+        const res = await axiosSecure.get(`/myPaymentHistories?email=${user?.email}`)
         return res.data;
     })
-    console.log(myPaymentHistory);
+    console.log(myPaymentHistories);
 
     return (
         <div>
-            <Title title='My Selected Classes'></Title>
+            <Title title='Payment History'></Title>
             <div className="overflow-x-auto">
                 <table className="table">
                     {/* head */}
@@ -25,27 +26,34 @@ const PaymentHistory = () => {
                         <tr className='text-lg bg-amber-50 text-center'>
                             <th>SL No.</th>
                             <th>Details</th>
-                            <th>Date</th>
+                            <th>Transaction ID</th>
+                            <th>Payment Date</th>
                             <th>Price</th>
                             <th>Status</th>
                         </tr>
                     </thead>
                     <tbody>
                         {
-                            selectedClasses.map((selectedClass, i) => <tr className="hover text-center">
+                            myPaymentHistories.map((myPaymentHistory, i) => <tr key={myPaymentHistory._id} className="hover text-center">
                                 <th>{i + 1}</th>
-                                <td>{className}</td>
-                                <td>{instructorName}</td>
-                                <td>${price}</td>
                                 <td>
-                                    <Link onClick={() => setModal(true)} className="btn btn-square btn-neutral btn-sm">Pay</Link>
+                                    <div className="flex items-center space-x-3">
+                                        <div className="avatar">
+                                            <div className="mask mask-squircle w-12 h-12">
+                                                <img src={myPaymentHistory.classImage} alt="Avatar Tailwind CSS Component" />
+                                            </div>
+                                        </div>
+                                        <div>
+                                            <div className="font-bold">{myPaymentHistory.className}</div>
+                                            <div className="text-sm opacity-70">{myPaymentHistory.instructorName}</div>
+                                        </div>
+                                    </div>
                                 </td>
-                                <td>
-                                    <button onClick={() => handleDelete(_id)} className="btn btn-circle btn-outline btn-sm">
-                                        <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" /></svg>
-                                    </button>
-                                </td>
-                                <Payment isOpen={modal} closeModal={closeModal} selectedClass={selectedClass} />
+                                <td>{myPaymentHistory.transactionId}</td>
+                                <td>{myPaymentHistory.date}</td>
+                                <td>${myPaymentHistory.price}</td>
+                                <td>{myPaymentHistory.status}</td>
+
                             </tr>)
                         }
 
